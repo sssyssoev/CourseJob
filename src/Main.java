c6cf1a5054cdb3ec0b1f83e8ed6829ad1dc7b1a5
ssyssoev <ssyssoev@mail.ru>
2023-08-01 12:06:59 +0300

    Первый коммит для 4-го вопроса экзамена

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+
+public class Main {
+    public static void main(String[] args) {
+        String str = "fghdjgthcrs";
+        int strlen = str.length();
+        if (strlen % 2 != 0) {
+            str = str + '0';
+            strlen = strlen + 1;
+
+
+            int strlen2 = strlen / 2;
+            System.out.println("strlen =" + strlen);
+            System.out.println("strlen/2 =   " + strlen2);
+
+            for (int i = 0; i <= strlen2-1; i++) {
+                System.out.println(str.charAt(i));
+            }
+            System.out.println("      ");
+            for (int i = strlen2; i <= strlen-1; i++) {
+                System.out.println(str.charAt(i));
+            }
+
+
+        }
+    }
+}
